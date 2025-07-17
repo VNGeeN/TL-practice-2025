2bdba72187e8c4dcf09d750baddbcfe8305abbe8
kritack <vkolian.kritack1912@gmail.com>
2025-07-17 20:30:06 +0300

    17.07.25 live coding

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder( args );
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,7 +8,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if ( app.Environment.IsDevelopment() )
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -21,24 +21,24 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet( "/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range( 1, 5 ).Select( index =>
         new WeatherForecast
         (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
+            DateOnly.FromDateTime( DateTime.Now.AddDays( index ) ),
+            Random.Shared.Next( -20, 55 ),
+            summaries[ Random.Shared.Next( summaries.Length ) ]
+        ) )
         .ToArray();
     return forecast;
-})
-.WithName("GetWeatherForecast")
+} )
+.WithName( "GetWeatherForecast" )
 .WithOpenApi();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+record WeatherForecast( DateOnly Date, int TemperatureC, string? Summary )
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 32 + ( int )( TemperatureC / 0.5556 );
 }
