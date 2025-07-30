a99aad765a0fe870b04e8e81acc4a5b373c09605
kritack <vkolian.kritack1912@gmail.com>
2025-07-31 01:12:01 +0300

    убрал дефолтные значения в OrderData

--- OrderManagement/OrderData.cs
+++ OrderManagement/OrderData.cs
@@ -1,7 +1,7 @@
 public record OrderData
 {
-    public string Product { get; set; } = "";
+    public string Product { get; set; }
     public int Count { get; set; }
-    public string Name { get; set; } = "";
-    public string Address { get; set; } = "";
+    public string Name { get; set; }
+    public string Address { get; set; }
 }
